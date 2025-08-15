7f20d34574ac6624411743d9978a7b74d3955cff
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2025-08-16 00:12:12 +0600

    done: #issue-1 project setup

--- backup/src/lecturePost/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import lecture
-# Register your models here.
-
-class lectureAdmin(admin.ModelAdmin):
-	class Meta:
-		model = lecture
-
-admin.site.register(lecture, lectureAdmin)
--- backup/src/noticeBoard/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class notice(models.Model):
-	title = models.CharField(max_length=120)
-	image = models.
