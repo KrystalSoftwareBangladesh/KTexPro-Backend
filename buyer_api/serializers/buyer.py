ea33915ffbc4c15bb948d3e6a852b613020320f4
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2025-11-03 00:27:05 +0600

    done: issue#14 - add new contact to the buyer

--- buyer_api/serializers/buyer.py
+++ buyer_api/serializers/buyer.py
@@ -9,48 +9,45 @@ User = get_user_model()
 
 class BuyerSerializer(serializers.ModelSerializer):
     contact_persons = UserProfileSerializer(many=True)
-    # contact_persons_ids = serializers.PrimaryKeyRelatedField(
-    #     many=True,
-    #     queryset=User.objects.all(),
-    #     write_only=True,
-    #     source="contact_persons"
-    # )
+    created_by_name = serializers.CharField(
+        source="created_by.full_name", read_only=True)
+    updated_by_name = serializers.CharField(
+        source="updated_by.full_name", read_only=True)
 
     class Meta:
         model = Buyer
         fields = [
-            "id",
-            "name",
-            "industry",
-            "website",
-            "email",
-            "phone_number",
-            "billing_address",
+            "id", "name", "industry", "website",
+            "email", "phone_number", "billing_address",
             "contact_persons",
-            # "contact_persons_ids",
-            "created_at",
+            "created_at", "created_by", "updated_by",
+            "created_by_name", "updated_by_name",
         ]
         read_only_fields = ("id", "created_at")
 
     def create(self, validated_data):
-        print("Welcome to create buyer serializer...")
         contact_persons_data = validated_data.pop("contact_persons", [])
         buyer = Buyer.objects.create(**validated_data)
+
         for person_data in contact_persons_data:
             user = UserProfileSerializer().create(person_data)
-            print("User...", user)
             buyer.contact_persons.add(user)
+
         return buyer
 
     def update(self, instance, validated_data):
         contact_persons_data = validated_data.pop("contact_persons", None)
+
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
+
         instance.save()
 
         if contact_persons_data is not None:
             instance.contact_persons.clear()
+
             for person_data in contact_persons_data:
                 user = UserProfileSerializer().create(person_data)
                 instance.contact_persons.add(user)
+
         return instance
--- buyer_api/views/v1/buyer.py
+++ buyer_api/views/v1/buyer.py
@@ -1,12 +1,44 @@
-from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import viewsets
+from rest_framework import status
 
 from buyer_api.models import Buyer
 
-from buyer_api.serializers.buyer import BuyerSerializer
+from buyer_api.serializers import BuyerSerializer
+from user_api.serializers import UserProfileSerializer
 
 
 class BuyerViewSet(viewsets.ModelViewSet):
     queryset = Buyer.objects.all().order_by("name")
     serializer_class = BuyerSerializer
     permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(
+            created_by=self.request.user,
+            updated_by=self.request.user
+        )
+
+    def perform_update(self, serializer):
+        serializer.save(updated_by=self.request.user)
+
+    @action(detail=True, methods=["post"], url_path="add-contact-persons")
+    def add_contact_person(self, request, pk=None):
+        buyer = self.get_object()
+
+        serializer = UserProfileSerializer(data=request.data, many=True)
+        serializer.is_valid(raise_exception=True)
+
+        created_users = []
+
+        for person_data in serializer.validated_data:
+            user = UserProfileSerializer().create(person_data)
+            buyer.contact_persons.add(user)
+            created_users.append(user)
+
+        buyer.save()
+        buyer_data = BuyerSerializer(buyer).data
+
+        return Response(buyer_data, status=status.HTTP_201_CREATED)
