0b6384d118d7239073582bcf3637de0277ddf01e
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2025-11-03 15:58:33 +0600

    Configure static and media file settings

--- KTexPro/settings.py
+++ KTexPro/settings.py
@@ -154,11 +154,11 @@ USE_I18N = True
 
 USE_TZ = True
 
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
-
-STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
